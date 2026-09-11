d1f87866706367be9f006f5d5d29246bb065057e
lorena leandro <lorenaleandro855@gmail.com>
2026-09-11 13:22:10 -0300

    variáveis if, else, elif
    Fix indentation and syntax errors in age and grade checks.

--- teste.py
+++ teste.py
@@ -1,3 +1,4 @@
+#revisando variáveis
 print("Olá, mundo!")
 
 nome = "Lorena"
@@ -12,3 +13,18 @@ numero2 = 10
 resultado = numero1 + numero2
 
 print(resultado)
+
+#maior ou menor de idade: if=se, else=senão
+idade = 16
+if idade>=18:
+  print("Maior de idade")
+else
+print("Menor de idade")
+
+nota = 5
+if nota>=9:
+  print("Excelente")
+elif nota>=6:
+  print("Aprovado")
+else
+print("Reprovado")
